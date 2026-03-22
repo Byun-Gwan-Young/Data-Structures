2168f8d1a3528429b94a03858644aa7cfb050f96
Byun-gwan-young <rhksdud0228@naver.com>
2026-03-22 21:34:49 +0900

    Update my_math.c

--- week2/2/my_math.c
+++ week2/2/my_math.c
@@ -14,5 +14,8 @@ int gcd(int a, int b) {
 
 int lcm(int a, int b) {
     if (a == 0 || b == 0) return 0;
-    return abs(a) / gcd(a, b) * abs(b);  // 오버플로우 방지
+    return abs(a) / gcd(a, b) * abs(b);
+    // a * b / gcd 순서로 하면 a * b 가 int 범위 초과할 수 있음
+    // a / gcd * b 순서로 하면 먼저 나눠서 숫자를 줄인 후 곱함
+    // gcd는 항상 a의 약수이므로 a / gcd 는 나머지 없이 나누어 떨어짐
 }
