0bedd1347eb3fdaff066e889abace9382a9064dc
Byun-gwan-young <rhksdud0228@naver.com>
2026-05-16 16:44:28 +0900

    Update LinkedList.h

--- Week11/LinkedList/LinkedList.h
+++ Week11/LinkedList/LinkedList.h
@@ -21,5 +21,9 @@ extern LinkedList* insertItemLinkedList(LinkedList* li, NodeType* pre, int item)
 extern LinkedList* insertAtLinkedList(LinkedList* li, int at, int item);
 extern int deleteFirstLinkedList(LinkedList* li);
 extern int deleteLastLinkedList(LinkedList* li);
+extern int deleteItemLinkedList(LinkedList* li, int item);
 extern int deleteAtLinkedList(LinkedList* li, int at);
+extern int getItemLinkedList(LinkedList* li, int pos);
+extern int replaceItemLinkedList(LinkedList* li, int pos, int item);
+extern int nextItemLinkedList(LinkedList* li, NodeType* pre);
 extern int printLinkedList(LinkedList* li);
